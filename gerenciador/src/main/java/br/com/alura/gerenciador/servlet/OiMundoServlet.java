8b88f61ba254f4518dc5e2fe3b630669d6966f8d
apazze <apazze@users.noreply.github.com>
2022-05-02 11:00:49 -0300

    Adicionando usos da classe HttpServlet junto do Servidor Web Apache Tomcat V9.
    protocolo://ip:porta/contexto/recurso

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -14,12 +14,15 @@ public class OiMundoServlet extends HttpServlet {
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
 		PrintWriter out = resp.getWriter();
 		out.println("<html>");
 		out.println("<body>");
 		out.println("oi mundo, parabens vc escreveu o primeiro servlets.");
 		out.println("</body>");
 		out.println("</html>");
+		
+		System.out.println("o servlet OiMundoServlet foi chamado");
 
 	}
 }
