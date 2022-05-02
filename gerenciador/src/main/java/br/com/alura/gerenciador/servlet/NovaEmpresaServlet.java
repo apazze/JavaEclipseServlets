fe0b794653553203e7f818476eb03a925f2978af
apazze <apazze@users.noreply.github.com>
2022-05-02 16:15:14 -0300

    Adicionando mais usos de JSP. Mas que pode ser um pesadelo para manutenção como está.

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -3,6 +3,7 @@ package br.com.alura.gerenciador.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,9 +23,23 @@ public class NovaEmpresaServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Cadastrando nova empresa");
 		String nomeEmpresa = request.getParameter("nome");
+		Empresa empresa = new Empresa();
+		empresa.setNome(nomeEmpresa);
 		
-		PrintWriter out = response.getWriter();
-		out.println("<html><body> Empresa "+ nomeEmpresa +" cadastrada com sucesso!</body></html>");
+		Banco banco = new Banco();
+		banco.adiciona(empresa);
+		
+		/* Movido para novaEmpresaCriada.jsp
+		 * PrintWriter out = response.getWriter(); 
+		 * out.println("<html><body> Empresa "+ nomeEmpresa + " cadastrada com sucesso!</body></html>");
+		 */
+		
+		//Chamar o JSP
+		//HttpServletRequest através do método getRequestDispatcher().
+		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
+		request.setAttribute("empresa", empresa.getNome());
+		rd.forward(request, response);
+		
 		
 	}
 
