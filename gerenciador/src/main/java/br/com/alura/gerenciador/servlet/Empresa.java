7487f864ac7fb843893000c60c6216349c7f951a
apazze <apazze@users.noreply.github.com>
2022-05-03 12:07:37 -0300

    Adicionando uso da taglib fmt (formatação) para classe Date.
    O uso de Servlets e JSP é baixo nível. Spring MVC já alto nível e
    abstrai muita coisa.

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/Empresa.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/Empresa.java
@@ -1,9 +1,12 @@
 package br.com.alura.gerenciador.servlet;
 
+import java.util.Date;
+
 public class Empresa {
 	
 	private Integer id;
 	private String nome;
+	private Date dataAbertura = new Date();
 	
 	public Integer getId() {
 		return id;
@@ -20,6 +23,12 @@ public class Empresa {
 	@Override
 	public String toString() {
 		return this.nome;
+	}
+	public Date getDataAbertura() {
+		return dataAbertura;
+	}
+	public void setDataAbertura(Date dataAbertura) {
+		this.dataAbertura = dataAbertura;
 	}
 	
 }
--- gerenciador/src/main/webapp/formNovaEmpresa.jsp
+++ gerenciador/src/main/webapp/formNovaEmpresa.jsp
@@ -12,9 +12,12 @@
 
 <form action="${linkServletNovaEmpresa}" method="post">
 	Nome:<input type="text" name="nome" />
+	Data Abertura:<input type="text" name="data" />
 	<input type="submit" />
 </form>
 
-
+<c:forEach var="i" begin="1" end="10" step="2">
+       ${i} <br />
+     </c:forEach>
 </body>
 </html>
