060d673d12817426de9867839a18eba33d93c9f7
apazze <apazze@users.noreply.github.com>
2022-05-03 21:17:57 -0300

    Tomcat é um Middleware (intermediário) ou um Servlet Container que possui o método main e instancia o objeto da minha classe OiMundoServlet que estende Servlet. Recebe a requisição http e repassa ao objeto Servlet.

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -9,8 +9,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns="/oi")
+//@WebServlet(urlPatterns="/oi") web.xml é um método antigo, usar anotação!
 public class OiMundoServlet extends HttpServlet {
+	
+	public OiMundoServlet() {
+		System.out.println("Criando Oi Mundo Servlet");
+	}
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
