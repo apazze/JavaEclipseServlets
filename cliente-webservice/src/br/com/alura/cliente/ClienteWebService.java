4be67406c708753ed4cd1c75073fab69dbc2ed94
apazze <apazze@users.noreply.github.com>
2022-05-05 10:38:42 -0300

    Aplicação transformada em WebService, retornando XML e/ou Json ao invés de html para servir à todos os clientes. Os frameworks trabalham com Json (i.e. Angular).
    Adicionando usos de XML e Json. Adicionando projeto para simular um
    cliente do Web Service.

---
+++ cliente-webservice/src/br/com/alura/cliente/ClienteWebService.java
@@ -0,0 +1,19 @@
+package br.com.alura.cliente;
+
+import org.apache.http.client.fluent.Request;
+
+public class ClienteWebService {
+
+	public static void main(String[] args) throws Exception{
+
+		String conteudo = Request
+			.Post("http://localhost:8080/gerenciador/empresas")
+			.execute()
+			.returnContent()
+			.asString();
+		
+		System.out.println(conteudo);
+		
+	}
+
+}
